f043869c342c68dfed133cb0240e815d9fdf2e7c
Kevin Chen <usa.kevin@gmail.com>
2021-07-30 16:46:43 -0500

    added filenames

--- background.js
+++ background.js
@@ -1,17 +1,22 @@
 let downloadElement = document.getElementById('download');
- 
+let re = new RegExp(/(.mp4|.avi|.mpg|.wmv|.mov|.qt|.3gp|.wma|.mp3|.m4v)/i);
 chrome.storage.local.get(["panoptoLink"], (data) => {
+            chrome.storage.local.remove(["panoptoLink"]);
     if (data.panoptoLink !== undefined) {
         chrome.storage.local.remove(["panoptoLink"]);
-        downloadElement.innerHTML = `
-        <button type="button" id="button">
-            Download
-        </button>`
-        let button = document.getElementById('button');
-        button.addEventListener('click', ()=> {
-            chrome.downloads.download({url: data.panoptoLink});
-        })
+        chrome.storage.local.get(["panoptoTitle"], (title) => {
+            console.log(data);
+            console.log(title);
+            chrome.storage.local.remove(["panoptoTitle"]);
+            downloadElement.innerHTML = `
+            <button type="button" id="button">
+                Download
+            </button>`
+            let button = document.getElementById('button');
+            button.addEventListener('click', ()=> {
+                let videoExtension = data.panoptoLink.match(re)[0];
+                chrome.downloads.download({url: data.panoptoLink, filename: `${title.panoptoTitle}${videoExtension}`});
+            });
+        });
     }
 });
-
-
--- content.js
+++ content.js
@@ -1,18 +1,13 @@
-//myStorage = window.sessionStorage;
-
 
 window.onload = function () {
     let videoElement = document.getElementById('primaryVideo');
-
+    let videoTitle = document.getElementById('deliveryTitle');
     videoElement.addEventListener('loadeddata', () => {
         if (videoElement.readyState > 0) {
             let videoLink = document.getElementById('primaryVideo').src;
             if (typeof videoLink === 'string') {
-                chrome.storage.local.set({ 'panoptoLink': videoLink }, () => {
-                    chrome.storage.local.get(["panoptoLink"], (result) => {
-                        console.log(result);
-                    });
-                });
+                chrome.storage.local.set({ 'panoptoLink': videoLink });
+                chrome.storage.local.set({ 'panoptoTitle': videoTitle.innerText });
             }
         }
     });
